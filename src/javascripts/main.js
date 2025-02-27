16456cb9513e152a43fb0bd9152dfafe2d084a28
Miladybug <emmarj24601@gmail.com>
2025-02-26 17:18:09 -0700

    Moved components back to main files, style updates

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,87 +1,87 @@
 //TODO - Your ES6 JavaScript code (if any) goes here
 import "bootstrap";
 
-class Head extends HTMLElement {
-    constructor() {
-        super();
-    }
+// class Head extends HTMLElement {
+//     constructor() {
+//         super();
+//     }
 
-    connectedCallback() {
-        this.innerHTML = `
-        <head>
-            <meta charset="utf-8">
-            <title>Sariah Solomon</title>
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            <meta name="robots" content="index,follow">
-            <meta name="description" content="Sariah Solomon's Editing Portfolio">
-            <link rel="preconnect" href="https://fonts.googleapis.com">
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-            <link href="https://fonts.googleapis.com/css2?family=Alegreya:ital,wght@0,400..900;1,400..900&family=Belleza&family=Charm:wght@400;700&display=swap" rel="stylesheet">
-            <link rel="stylesheet" href="./stylesheets/main.scss">
-        </head>
-        `
-    }
-}
+//     connectedCallback() {
+//         this.innerHTML = `
+//         <head>
+//             <meta charset="utf-8">
+//             <title>Sariah Solomon</title>
+//             <meta name="viewport" content="width=device-width, initial-scale=1">
+//             <meta name="robots" content="index,follow">
+//             <meta name="description" content="Sariah Solomon's Editing Portfolio">
+//             <link rel="preconnect" href="https://fonts.googleapis.com">
+//             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+//             <link href="https://fonts.googleapis.com/css2?family=Alegreya:ital,wght@0,400..900;1,400..900&family=Belleza&family=Charm:wght@400;700&display=swap" rel="stylesheet">
+//             <link rel="stylesheet" href="./stylesheets/main.scss">
+//         </head>
+//         `
+//     }
+// }
 
-class Header extends HTMLElement {
-    constructor() {
-        super();
-    }
+// class Header extends HTMLElement {
+//     constructor() {
+//         super();
+//     }
 
-    connectedCallback() {
-        this.innerHTML = `
-        <header>
-            <!-- Add main header with title and image? -->
-            <nav class="navbar navbar-expand-lg bg-primary shadow fixed-top">
-                <a class="navbar-brand" id="main-logo" href="/index.html">
-                    <i class="fa-solid fa-pen-nib"></i>
-                    <span class="text-light">Sariah Solomon</span>
-                </a>
-                <button class="navbar-toggler m-1" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <i class="fa-solid fa-bars"></i>
-                </button>
-                <div class="collapse navbar-collapse bg-primary text-light pb-2" id="navbarNav">
-                    <ul class="navbar-nav text-nowrap text-light">
-                    <li class="nav-item px-3 mt-2">
-                        <a class="nav-link text-light" href="/index.html">Home</a>
-                    </li>
-                    <li class="nav-item px-3 mt-2">
-                        <a class="nav-link text-light" href="/blog.html">Blog</a>
-                    </li>
-                    <li class="nav-item px-3 mt-2">
-                        <a class="nav-link text-light" href="/about.html">About</a>
-                    </li>
-                    <li class="nav-item px-3 mt-2">
-                        <a class="nav-link text-light" href="/portfolio.html">Portfolio</a>
-                    </li>
-                    <li class="nav-item px-3 mt-2">
-                        <a class="nav-link text-light" href="/services.html">Services</a>
-                    </li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
-        `
-    }
-}
+//     connectedCallback() {
+//         this.innerHTML = `
+//         <header>
+//             <!-- Add main header with title and image? -->
+//             <nav class="navbar navbar-expand-lg bg-primary shadow fixed-top">
+//                 <a class="navbar-brand" id="main-logo" href="/index.html">
+//                     <i class="fa-solid fa-pen-nib"></i>
+//                     <span class="text-light">Sariah Solomon</span>
+//                 </a>
+//                 <button class="navbar-toggler m-1" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+//                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+//                     <i class="fa-solid fa-bars"></i>
+//                 </button>
+//                 <div class="collapse navbar-collapse bg-primary text-light pb-2" id="navbarNav">
+//                     <ul class="navbar-nav text-nowrap text-light">
+//                     <li class="nav-item px-3 mt-2">
+//                         <a class="nav-link text-light" href="/index.html">Home</a>
+//                     </li>
+//                     <li class="nav-item px-3 mt-2">
+//                         <a class="nav-link text-light" href="/blog.html">Blog</a>
+//                     </li>
+//                     <li class="nav-item px-3 mt-2">
+//                         <a class="nav-link text-light" href="/about.html">About</a>
+//                     </li>
+//                     <li class="nav-item px-3 mt-2">
+//                         <a class="nav-link text-light" href="/portfolio.html">Portfolio</a>
+//                     </li>
+//                     <li class="nav-item px-3 mt-2">
+//                         <a class="nav-link text-light" href="/services.html">Services</a>
+//                     </li>
+//                     </ul>
+//                 </div>
+//             </nav>
+//         </header>
+//         `
+//     }
+// }
 
-class Footer extends HTMLElement {
-    constructor() {
-        super();
-    }
+// class Footer extends HTMLElement {
+//     constructor() {
+//         super();
+//     }
 
-    connectedCallback() {
-        this.innerHTML = `
-        <footer class="text-center bg-light text-dark p-2 m-0">
-            <p>
-            <span>&copy;</span><span>2025 All rights reserved. <a href="#">Privacy Policy</a> | <a href="#">Terms of Use</a></span>
-            </p>
-        </footer>
-        `
-    }
-}
+//     connectedCallback() {
+//         this.innerHTML = `
+//         <footer class="text-center bg-light text-dark p-2 m-0">
+//             <p>
+//             <span>&copy;</span><span>2025 All rights reserved. <a href="#">Privacy Policy</a> | <a href="#">Terms of Use</a></span>
+//             </p>
+//         </footer>
+//         `
+//     }
+// }
 
-customElements.define('head-component', Head);
-customElements.define('header-component', Header);
-customElements.define('footer-component', Footer);
+// customElements.define('head-component', Head);
+// customElements.define('header-component', Header);
+// customElements.define('footer-component', Footer);
